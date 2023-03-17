b79d54bc994e6a56f592a3b852e8f08f38f97f21
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-03-17 15:50:51 -0400

    feeling loopy

---
+++ js/for_loops.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* program to generate a multiplication table
+upto a range */
+
+// // take number input from the user
+// var number = parseInt(prompt('Enter an integer: '));
+//
+// // take range input from the user
+// var range = parseInt(prompt('Enter a range: '));
+// function showMultiplicationTable() {
+// //creating a multiplication table
+//     for (let i = 1; i <= range; i++) {
+//         console.log("%d * %d = %d \n", number, i, i * number);
+//     }
+// }
+// showMultiplicationTable()
+//
+ for (let i = 1; i <=10; i++){
+var numRandom = Math.floor(Math.random() * (200 - 20) + 20);
+    if (numRandom % 2 === 0)
+        console.log(numRandom + " is even")
+    else (numRandom % 2 !== 0)
+        console.log(numRandom + " is odd")
+}
+
+
+for(var i = 0; i < 10; i++){
+    var str = "";
+    for(var j = 0; j < i; j++){
+        str = str + i;
+    }
+    console.log(str);
+}
