515c157bd1e1b3baf92ddede3d8555a7794a4486
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-03-22 11:59:12 -0400

    Who wrote this

--- js/circle.js
+++ js/circle.js
@@ -3,28 +3,26 @@
 
     // create a circle object
     var circle = {
-        radius: 3,
+        radius: 5,
 
-        getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+        getArea: function areaOfCircle (radius, pi) {
+            return Math.PI * Math.pow(circle.radius,2)
+            // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+        Math.round(Math.PI * Math.pow(circle.radius, 2))
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
         }
     };
-
+console.log(circle.getArea())
     // log info about the circle
     console.log("Raw circle information");
-    circle.logInfo(false);
+    circle.logInfo(true);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
 
