90cb5a5c5c0cb27a3d1045006c4c9121a2888c3b
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-04-20 12:16:13 -0400

    Konami code red

--- js/jquery.js
+++ js/jquery.js
@@ -1,15 +1,16 @@
-$("h1").click(function () {
-    $("h1").css('background-color', 'red');
-});
-$("p").dblclick(function (e) {
-    $("p").css('font-size', '18px');
-});
-
-$('li').hover(
-    function() {
-        $(this).css('color', 'red');
-    },
-    function() {
-        $(this).css('color', 'black');
-    }
-);
+// $("h1").click(function () {
+//     $("h1").css('background-color', 'red');
+// });
+// $("p").dblclick(function (e) {
+//     $("p").css('font-size', '18px');
+// });
+//
+// $('li').hover(
+//     function() {
+//         $(this).css('color', 'red');
+//     },
+//     function() {
+//         $(this).css('color', 'black');
+//     }
+// );
+// prompt("what's the code")
