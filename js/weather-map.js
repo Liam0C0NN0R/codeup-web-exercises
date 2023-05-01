e0a13721ef29a135d0d72675dac6eae58e83f04c
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-01 11:14:29 -0400

    added navbar, no map yet

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,8 +1,23 @@
 (function () {
+    "use strict"
 import{ openWeatherKey, mapboxToken} from 'js/keys.js';
 
 
 
 
+    mapboxgl.accessToken = mapboxToken;
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 10,
+        center: ([38.24954727722081, -85.65776736418078])
+    });
 
+    //marker for the map
+    const marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        //this is where the marker starts
+        .setLngLat([38.24954727722081, -85.65776736418078])
+        .addTo(map);
 })();
