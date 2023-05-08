d75bd03e62a0361f16ddff9e60ae181dfdf1bfa9
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-08 12:59:30 -0400

    promises

---
+++ js/promises.js
@@ -0,0 +1,51 @@
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(posts) {
+//         posts.forEach(function(post) {
+//             console.log(post);
+//         });
+//     })
+//     .catch(function(error) {
+//         console.error(error);
+//     });
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => {
+        posts.forEach(post => console.log(post));
+    })
+    .catch(error => console.error(error));
+
+
+
+// fetch(url, {headers: {'Authorization': "githubKey"}})
+
+function getLastCommitDate(username) {
+    return fetch(`https://api.github.com/users/${username}/events`)
+        .then(response => response.json())
+        .then(events => {
+            const lastPushEvent = events.find(event => event.type === "PushEvent");
+            if (lastPushEvent) {
+                return lastPushEvent.created_at;
+            } else {
+                throw new Error("No push events found for this user.");
+            }
+        })
+        .then(dateString => new Date(dateString).toDateString());
+}
+
+console.log(getLastCommitDate('Liam0C0NN0R'));
+
+function wait(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(milliseconds);
+        }, milliseconds);
+    });
+}
+
+wait(1000).then(milliseconds => console.log(`You'll see this after ${milliseconds} milliseconds`));
+wait(3000).then(milliseconds => console.log(`You'll see this after ${milliseconds} milliseconds`));
