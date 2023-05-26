a6ca8f3135ba7f6156186153d6d527eb8d879739
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-26 02:40:38 -0400

    initial Carousels and movie posters loading and moving properly.

--- js/Movie.mjs
+++ js/Movie.mjs
@@ -1,4 +1,6 @@
 import Glide from '@glidejs/glide';
+import '@glidejs/glide/dist/css/glide.core.min.css';
+import '@glidejs/glide/dist/css/glide.theme.min.css';
 import { tmdbToken } from './keys.mjs';
 (function () {
     "use strict";
@@ -45,29 +47,47 @@ import { tmdbToken } from './keys.mjs';
         // Initialize Glide for the carousel
         const glide = new Glide(carouselId, {
             type: 'carousel',
+            autoplay: true,
+            animationDuration: 6000,
+            animationTimingFunc: 'cubic-bezier(1,1,.71,.71)',
+            gap: 20,
             startAt: 3,
             perView: 5,
+            peek: { before: 10, after: 10 },
             focusAt: 'center',
-            direction: carouselId === '#carousel1' ? 'ltr' : 'rtl' // Make carousel1 slide to left and carousel2 slide to right
+            direction: carouselId === '#carousel1' ? 'rtl' : 'ltr' // Make carousel1 slide to left and carousel2 slide to right
         }).mount();
 
         // Update glide with new slides
         glide.update();
     }
+    // This function fetches new movies from the TMDB API
+    function fetchNewMovies(page = 1) {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${tmdbToken}`
+            }
+        };
+
+        return fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`, options)
+            .then(response => response.json())
+            .catch(err => console.error(err));
+    }
+
+// This function takes a carousel ID and a movies data array, then creates and appends new slides to the corresponding carousel
+    function appendMoviesToCarousel(carouselId, movies) {
+        const carouselSlides = document.querySelector(`${carouselId} .glide__slides`);
+
+        movies.results.forEach(movie => {
+            const li = document.createElement('li');
+            li.classList.add('glide__slide');
+            li.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">`;
+            carouselSlides.appendChild(li);
+        });
+    }
 })();
-
-
-
-
-
-
-
-
-
-
-// TODO 3. Set up next/previous buttons that call .goNext() and .goPrev() on both C1 and C2.
-//  4. Set up an event listener for when C1 reaches the end of its slides. In that listener, make an API call for 20 more posters and append them to C1. Then grab the 20 posters currently in C1 and append them to C2. This will make it appear C1 flows into C2.
-//  5. In the event listener, call .goNext() on C1 and C2 to continue their slide animation uninterrupted after the new posters are added.
 
 
 
