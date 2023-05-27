da5099d74d6609edf17e3cdcf261c4151e0d2a1b
Liam O'Connor <liam.anson.oconnor@gmail.com>
2023-05-26 21:53:51 -0400

    giving up on Glide.js, codes don't line up well. switching to Slick.

--- js/Movie.mjs
+++ js/Movie.mjs
@@ -1,19 +1,17 @@
-import Glide from '@glidejs/glide';
-import '@glidejs/glide/dist/css/glide.core.min.css';
-import '@glidejs/glide/dist/css/glide.theme.min.css';
+import $ from 'jquery';
+import 'slick-carousel';
+
+import 'slick-carousel/slick/slick.min.js';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import { tmdbToken } from './keys.mjs';
+
 (function () {
     "use strict";
     const url = "https://admitted-fish-canidae.glitch.me//movies";
-
-
-//TODO: Create two Glide carousels, give them IDs of C1 and C2. Set them up with opposite directions (C1 slides left, C2 slides right).
-
-//TODO On page load, make an API call to TMDB to get 20 random movie posters. Append those to C1. Make another call to get 20 more posters and append those to C2.
-
-// Fetch a random page of posters for carousel1
-    const page1 = Math.floor(Math.random() * 10) + 1;
-    const page2 = Math.floor(Math.random() * 10) + 1;
+    let carouselData = [];
+    const carouselPages = [1, 2, 3];
     const options = {
         method: 'GET',
         headers: {
@@ -22,72 +20,58 @@ import { tmdbToken } from './keys.mjs';
         }
     };
 
+    // Fetch data for the initial 3 pages and append to carouselData
+    const fetchPromises = carouselPages.map(page => fetchMovies(page));
 
-    // Fetch movies for carousel 1
-    fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&include_video=false&language=en-US&page=${page1}&sort_by=popularity.desc`, options)
-        .then(response => response.json())
-        .then(data => generateSlides('#carousel1', data))
-        .catch(err => console.error(err));
+    // Once the initial 3 pages are fetched, populate the carousels
+    Promise.all(fetchPromises).then(moviesData => {
+        carouselData = [...moviesData];
+        generateSlides('#carousel1', carouselData[0]);
+        generateSlides('#carousel2', carouselData[1]);
+    });
 
-    // Fetch movies for carousel 2
-    fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&include_video=false&language=en-US&page=${page2}&sort_by=popularity.desc`, options)
-        .then(response => response.json())
-        .then(data => generateSlides('#carousel2', data))
-        .catch(err => console.error(err));
+    function fetchMovies(page) {
+        return fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`, options)
+            .then(response => response.json());
+    }
 
     function generateSlides(carouselId, movies) {
-        const carouselSlides = document.querySelector(`${carouselId} .glide__slides`);
+        const carouselDiv = document.querySelector(`${carouselId} div`);
         movies.results.forEach(movie => {
-            const li = document.createElement('li');
-            li.classList.add('glide__slide');
-            li.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">`;
-            carouselSlides.appendChild(li);
+            const img = document.createElement('img');
+            img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            img.alt = movie.title;
+            carouselDiv.appendChild(img);
         });
 
-        // Initialize Glide for the carousel
-        const glide = new Glide(carouselId, {
-            type: 'carousel',
-            autoplay: true,
-            animationDuration: 6000,
-            animationTimingFunc: 'cubic-bezier(1,1,.71,.71)',
-            gap: 20,
-            startAt: 3,
-            perView: 5,
-            peek: { before: 10, after: 10 },
-            focusAt: 'center',
-            direction: carouselId === '#carousel1' ? 'rtl' : 'ltr' // Make carousel1 slide to left and carousel2 slide to right
-        }).mount();
+        $(carouselId).slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 3,
+            rtl: carouselId === '#carousel1'
+        });
 
-        // Update glide with new slides
-        glide.update();
-    }
-    // This function fetches new movies from the TMDB API
-    function fetchNewMovies(page = 1) {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${tmdbToken}`
+        $(carouselId).on('afterChange', function(event, slick, currentSlide){
+            const slideCount = slick.slideCount;
+            if (currentSlide + slick.options.slidesToShow >= slideCount) {
+                // Fetch a new page and append to carouselData
+                const newPage = carouselData.length + 1;
+                fetchMovies(newPage)
+                    .then(data => {
+                        carouselData.push(data);
+                        appendMoviesToCarousel(carouselId, data);
+                    })
+                    .catch(err => console.error(err));
             }
-        };
-
-        return fetch(`https://api.themoviedb.org/3/movie/popular?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`, options)
-            .then(response => response.json())
-            .catch(err => console.error(err));
+        });
     }
 
-// This function takes a carousel ID and a movies data array, then creates and appends new slides to the corresponding carousel
     function appendMoviesToCarousel(carouselId, movies) {
-        const carouselSlides = document.querySelector(`${carouselId} .glide__slides`);
-
         movies.results.forEach(movie => {
-            const li = document.createElement('li');
-            li.classList.add('glide__slide');
-            li.innerHTML = `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">`;
-            carouselSlides.appendChild(li);
+            const img = document.createElement('img');
+            img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            img.alt = movie.title;
+            $(carouselId).slick('slickAdd', img.outerHTML);
         });
     }
 })();
-
-
-
